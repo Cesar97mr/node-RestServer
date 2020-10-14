8cefccc15978b541b38e5f4a47a8c0fd24934437
Cesar <Cesar24gas@gmail.com>
2020-10-14 20:20:40 +0200

    Subida de archivos

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,9 @@ const app = express();
 app.use(require('../routes/login'));
 app.use(require('../routes/usuario'));
 app.use(require('./categoria'));
-app.use(require('./producto'))
+app.use(require('./producto'));
+app.use(require('./upload'));
+app.use(require('./imagenes'));
+
 
 module.exports = app;
